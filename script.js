a2afdff812ffb85c97a8381230899bc1f388c7c5
Anukua John Brolin <118099420+john-brolin@users.noreply.github.com>
2023-03-23 13:16:43 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+let boy = document.getElementById("view-recipe")
+
+function alertUser(){
+    window.alert("welcome to my website, feel to select any recipe of your choice")
+}
+boy.onclick = alertUser;
